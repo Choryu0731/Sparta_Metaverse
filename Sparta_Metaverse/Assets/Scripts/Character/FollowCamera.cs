2388d4cffccc7231dbfaaaf1fba92e48744badfe
Choryu0731 <nabiiydg@naver.com>
2025-05-02 09:57:30 +0900

    feat. FollowCamera Script 추가

--- Sparta_Metaverse/Assets/Scripts/Character/FollowCamera.cs
+++ Sparta_Metaverse/Assets/Scripts/Character/FollowCamera.cs
@@ -6,14 +6,28 @@ public class FollowCamera : MonoBehaviour
 {
     public Transform target;
     float offsetX;
+    float offsetY;
 
     void Start()
     {
+        if (target == null)
+            return;
 
+        offsetX = transform.position.x - target.position.x;
+        offsetY = transform.position.y - target.position.y;
     }
 
     void Update()
     {
+        if (target == null)
+            return;
 
+        Vector3 newPos = new Vector3(
+            target.position.x + offsetX,
+            target.position.y + offsetY,
+            transform.position.z
+            );
+
+        transform.position = newPos;        
     }
 }
